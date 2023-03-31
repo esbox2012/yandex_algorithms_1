05b0146cbb52cf96a6f3485d1642c332b278dc2e
esbox2012 <erpalovpavel@gmail.com>
2023-03-31 17:00:36 +0500

    changed 1F

--- 1F.py
+++ 1F.py
@@ -1,12 +1,20 @@
-a,b,c,d=map(int,input().split())
-if a==c:
-  print(a,b+d)
-elif a==d:
-  print(a,b+c)
-elif b==c:
-  print(b,a+d)
-elif b==d:
-  print(b,a+c)
-else:
-  s=sorted([[a+c,max(b,d),(a+c)*max(b,d)],[a+d,max(b,c),(a+d)*max(b,c)],[b+c,max(a,d),(b+c)*max(a,d)],[b+d,max(a,c),(b+d)*max(a,c)]],key=lambda x:x[2])
-  print(s[0][0],s[0][1])
+def notebook(s: str) -> str:
+    a, b, c, d = map(int, s.split())
+    if a == c:
+        return f'{a} {b + d}'
+    elif a == d:
+        return f'{a} {b + c}'
+    elif b == c:
+        return f'{b} {a + d}'
+    elif b == d:
+        return f'{b} {a + c}'
+    else:
+        s = sorted([[a + c, max(b, d), (a + c) * max(b, d)], [a + d, max(b, c), (a + d) * max(b, c)],
+                    [b + c, max(a, d), (b + c) * max(a, d)], [b + d, max(a, c), (b + d) * max(a, c)]],
+                   key=lambda x: x[2])
+        return f'{s[0][0]} {s[0][1]}'
+
+
+if __name__ == '__main__':
+    assert notebook('10 2 2 10') == '10 4'
+    assert notebook('5 7 3 2') == '9 5'

99a2a62309a2f651fbd8d92b6ea76b2d850348b9
esbox2012 <erpalovpavel@gmail.com>
2023-03-31 20:31:56 +0500

    changed 1H

--- 1H.py
+++ 1H.py
@@ -1,12 +1,16 @@
-reader=open('input.txt','r')
-a,b,n,m=list(map(int,reader.read().rstrip().split('\n')))
-reader.close()
-l1,l2=[n+(n-1)*a,n+(n+1)*a],[m+(m-1)*b,m+(m+1)*b]
-res=-1
-if l1[0]<l2[1] and l1[1]>l2[0]:
-  res=[max(l1[0],l2[0]),min(l1[1],l2[1])]
-elif l1[0]==l2[1]:
-  res=[l1[0],l1[0]]
-elif l1[1]==l2[0]:
-  res=[l2[0],l2[0]]
-print(' '.join(list(map(str,res))) if res!=-1 else res)
+def metro(s: str) -> str:
+    a, b, n, m = list(map(int, s.split('\n')))
+    l1, l2 = [n + (n - 1) * a, n + (n + 1) * a], [m + (m - 1) * b, m + (m + 1) * b]
+    res = -1
+    if l1[0] < l2[1] and l1[1] > l2[0]:
+        res = [max(l1[0], l2[0]), min(l1[1], l2[1])]
+    elif l1[0] == l2[1]:
+        res = [l1[0], l1[0]]
+    elif l1[1] == l2[0]:
+        res = [l2[0], l2[0]]
+    return ' '.join(list(map(str, res))) if res != -1 else str(res)
+
+
+if __name__ == '__main__':
+    assert metro('1\n3\n3\n2') == '5 7'
+    assert metro('1\n5\n1\n2') == '-1'

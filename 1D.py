c56dfb220e1059324076a7a9acb9d9c29149a2f2
esbox2012 <erpalovpavel@gmail.com>
2023-03-31 16:37:36 +0500

    changed 1D

--- 1D.py
+++ 1D.py
@@ -1,13 +1,19 @@
-reader=open('input.txt','r')
-a,b,c=list(map(int,reader.read().rstrip().split('\n')))
-if c<0:
-  print('NO SOLUTION')
-elif a==0:
-  if b!=c**2:
-    print('NO SOLUTION')
-  else:
-    print('MANY SOLUTIONS')
-elif ((c**2-b)/a)%1>0:
-  print('NO SOLUTION')
-else:
-  print((c**2-b)//a)
+from typing import Union
+
+
+def solve(coef: str) -> Union[str, int]:
+    a, b, c = list(map(int, coef.split('\n')))
+    if c < 0:
+        return 'NO SOLUTION'
+    elif a == 0:
+        return 'NO SOLUTION' if b != c ** 2 else 'MANY SOLUTIONS'
+    elif ((c ** 2 - b) / a) % 1 > 0:
+        return 'NO SOLUTION'
+    else:
+        return (c ** 2 - b) // a
+
+
+if __name__ == '__main__':
+    assert (solve('1\n0\n0')) == 0
+    assert (solve('1\n2\n3')) == 7
+    assert (solve('1\n2\n-3')) == 'NO SOLUTION'

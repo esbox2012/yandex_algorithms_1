eaafbf8c9706bdb5321d55ab4e248691b2eea10b
esbox2012 <erpalovpavel@gmail.com>
2023-03-31 20:37:40 +0500

    changed 1I

--- 1I.py
+++ 1I.py
@@ -1,6 +1,10 @@
-reader=open('input.txt','r')
-a,b,c,d,e=list(map(int,reader.read().rstrip().split('\n')))
-reader.close()
-brick=sorted([a,b,c])
-window=sorted([d,e])
-print('YES') if brick[0]<=window[0] and brick[1]<=window[1] else print('NO')
+def castle(s: str) -> str:
+    a, b, c, d, e = list(map(int, s.split('\n')))
+    brick = sorted([a, b, c])
+    window = sorted([d, e])
+    return 'YES' if brick[0] <= window[0] and brick[1] <= window[1] else 'NO'
+
+
+if __name__ == '__main__':
+    assert castle('1\n1\n1\n1\n1') == 'YES'
+    assert castle('2\n2\n2\n1\n1') == 'NO'

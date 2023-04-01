01e56b72669a8722852d1a2f01dce47b6c0d742c
esbox2012 <erpalovpavel@gmail.com>
2023-04-01 23:16:16 +0500

    add test A2

---
+++ tests/A2_check_seq_test.py
@@ -0,0 +1,23 @@
+from unittest import TestCase, main
+from A2 import check_seq
+
+
+class CheckSeqTest(TestCase):
+    def test_increasing_seq(self):
+        self.assertEqual(check_seq('1 2 3'), 'YES')
+
+    def test_non_decreasing_seq(self):
+        self.assertEqual(check_seq('1 2 2'), 'NO')
+
+    def test_decreasing_se1(self):
+        self.assertEqual(check_seq('3 2 1'), 'NO')
+
+    def test_non_increasing_seq(self):
+        self.assertEqual(check_seq('3 2 2'), 'NO')
+
+    def test_const_seq(self):
+        self.assertEqual(check_seq('1 1 1'), 'NO')
+
+
+if __name__ == '__main__':
+    main()

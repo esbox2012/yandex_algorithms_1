f6c2a8798b91511a808b9d30092bc83ceac43932
esbox2012 <erpalovpavel@gmail.com>
2023-04-26 22:13:48 +0500

    correct D2 and tests D2

--- lecture_2/D2.py
+++ lecture_2/D2.py
@@ -1,4 +1,4 @@
-def counter(s: str) -> int:
+def bigger_than_neighbors(s: str) -> int:
     s = [int(i) for i in s.split()]
     count = 0
     for i in range(1, len(s) - 1):
@@ -8,8 +8,8 @@ def counter(s: str) -> int:
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as reader:
-        print(counter(reader.read().rstrip()))
+        print(bigger_than_neighbors(reader.read().rstrip()))
 
-    assert counter('1 2 3 4 5') == 0
-    assert counter('5 4 3 2 1') == 0
-    assert counter('1 5 1 5 1') == 2
+    assert bigger_than_neighbors('1 2 3 4 5') == 0
+    assert bigger_than_neighbors('5 4 3 2 1') == 0
+    assert bigger_than_neighbors('1 5 1 5 1') == 2
--- tests/D2_bigger_than_neighbors.py
+++ tests/D2_bigger_than_neighbors.py
@@ -1,29 +1,29 @@
 from unittest import TestCase, main
-from lecture_2.D2 import counter
+from lecture_2.D2 import bigger_than_neighbors
 
 
 class Bigger_than_neighbors_test(TestCase):
 
     def test_constant_seq(self):
-        self.assertEqual(counter('1 1 1 1 1'), 0)
+        self.assertEqual(bigger_than_neighbors('1 1 1 1 1'), 0)
 
     def test_increasing_seq(self):
-        self.assertEqual(counter('1 2 3 4 5'), 0)
+        self.assertEqual(bigger_than_neighbors('1 2 3 4 5'), 0)
 
     def test_decreasing_seq(self):
-        self.assertEqual(counter('5 4 3 2 1'), 0)
+        self.assertEqual(bigger_than_neighbors('5 4 3 2 1'), 0)
 
     def test_random_seq(self):
-        self.assertEqual(counter('1 3 1 3 1'), 2)
+        self.assertEqual(bigger_than_neighbors('1 3 1 3 1'), 2)
 
     def test_3_numbers(self):
-        self.assertEqual(counter('1 2 0'), 1)
+        self.assertEqual(bigger_than_neighbors('1 2 0'), 1)
 
     def test_2_numbers(self):
-        self.assertEqual(counter('1 2'), 0)
+        self.assertEqual(bigger_than_neighbors('1 2'), 0)
 
     def test_1_number(self):
-        self.assertEqual(counter('1'), 0)
+        self.assertEqual(bigger_than_neighbors('1'), 0)
 
 
 if __name__ == '__main__':
